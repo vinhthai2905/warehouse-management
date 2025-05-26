84634719a56f4f300cc5cfa34e0cde9dba5e2032
vinhthai2905 <vinhthai2905@aol.com>
2025-05-26 13:02:28 +0700

    interface: fixed url path

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home) 
+    path('', views.home),
+    path('export/', views.export_management, name='export'),
+    path('info/', views.product_info, name='product_info'),
 
 ]
--- products/views.py
+++ products/views.py
@@ -5,12 +5,21 @@ from django.shortcuts import render
 
 
 def home(request):
-    context = {}
-    section = request.GET.get('section')
-    if section == 'export-management':
-        context['show_export_management'] = True
-    elif section == 'inventory-management':
-        context['show_inventory_management'] = True
-    elif section == 'product-info':
-        context['show_product_info'] = True
-    return render(request, 'products/home.html', context)
+    # context = {}
+    # section = request.GET.get('section')
+    # if section == 'export-management':
+    #     context['show_export_management'] = True
+    # elif section == 'inventory-management':
+    #     context['show_inventory_management'] = True
+    # elif section == 'product-info':
+    #     context['show_product_info'] = True
+    return render(request, 'products/home.html')
+
+def export_management(request):
+    return render(request, 'products/export-management.html')
+
+def inventory_management(request):
+    return render(request, 'products/inventory-management.html')
+
+def product_info(request):
+    return render(request, 'products/product-info.html')
